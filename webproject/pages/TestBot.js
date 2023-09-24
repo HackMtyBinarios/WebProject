cbfcb318ddf1a6361c5e2ba5ca7081e0e36acace
NolbertoCastro <nolber0703@gmail.com>
2023-09-24 07:07:35 -0600

    Chat ready, conversations ready, but you cant scroll conversations, limitations pending

--- webproject/pages/TestBot.js
+++ webproject/pages/TestBot.js
@@ -1,49 +1,61 @@
 import React from 'react'
 import HeaderTest from './components/HeaderTest'
-import Lupa from '../Assets/lupa.svg'
 import { useState } from "react";
-import Image from 'next/image';
+
+function Conversation({ prompt, result }) {
+    return (
+      <section className='flex flex-col justify-end gap-5'>
+        <div className="w-full h-fit bg-Gris border-Negro border-2">
+          <p className='w-[424px] text-Negro p-5 whitespace-pre-line break-words font-chivo font-regular text-sm'>{prompt}</p>
+        </div>
+        <div className="w-full h-fit bg-Verde-Clarito border-Negro border-2">
+          <p className='w-[424px] text-Negro p-5 whitespace-pre-line break-words font-chivo font-regular text-sm'>{result}</p>
+        </div>
+      </section>
+    );
+}
 
 function TestBot() {
     const [questionInput, setquestionInput] = useState("");
-    const [result, setResult] = useState();
+    const [conversations, setConversations] = useState([]);
 
     async function onSubmit(event) {
         event.preventDefault();
         try {
-        const response = await fetch("/api/generate", {
-            method: "POST",
-            headers: {
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ question: questionInput }),
-        });
+            const response = await fetch("/api/generate", {
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ question: questionInput }),
+            });
 
-        const data = await response.json();
-        if (response.status !== 200) {
-            throw data.error || new Error(`Request failed with status ${response.status}`);
-        }
+            const data = await response.json();
+            if (response.status !== 200) {
+                throw data.error || new Error(`Request failed with status ${response.status}`);
+            }
 
-        setResult(data.result);
-        setquestionInput("");
+            const newConversation = <Conversation key={Math.random} prompt={questionInput} result={data.result} />;
+            setConversations((prevConversations) => [...prevConversations, newConversation]);
+
+
+            setquestionInput("");
         } catch(error) {
-        // Consider implementing your own error handling logic here
-        console.error(error);
-        alert(error.message);
+            // Consider implementing your own error handling logic here
+            console.error(error);
+            alert(error.message);
         }
     }
+
     return (
         <>
             <HeaderTest/>
             <section className='w-screen h-[88vh] flex justify-center'>
-                <section className='w-[50vw] h-full flex justify-center'>
-                    <section className='h-full flex flex-col justify-end py-12 gap-10'>
-                        <section>
-
+                <section className='w-[50vw] flex justify-center'>
+                    <section className='flex flex-col justify-end py-12 gap-10'>
+                        <section className='flex flex-col justify-end gap-7 overflow-auto' id='conversations'>
+                            {conversations}
                         </section>
-                        <div className="w-full bg-Verde-Clarito">
-                            <p className='w-[424px] text-Negro p-5 whitespace-pre-line break-words font-chivo font-regular text-sm'>{result}</p>
-                        </div>
                         <form onSubmit={onSubmit} className='flex items-center justify-center'>
                             <input
                                 type="text"
@@ -51,7 +63,7 @@ function TestBot() {
                                 placeholder="¿Con que te puedo ayudar el día de hoy?"
                                 value={questionInput}
                                 onChange={(e) => setquestionInput(e.target.value)}
-                                autocomplete="off"
+                                autoComplete="off"
                                 className='promptInput'
                             />
                                 <button type="submit" value="" className='AskButton'>
@@ -69,6 +81,5 @@ function TestBot() {
         </>
     )
 }
-
 
 export default TestBot
